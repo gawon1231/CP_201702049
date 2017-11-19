3650696faecd60a8b66ba3ef24b7822c1f6ed06e
gawon1231 <gawon1231@hanmail.net>
2017-11-19 15:42:02 +0900

    first assignment

---
+++ [CP1]HW01_201702049_이가원/src/이가원_201702049_W01/이가원_1주차과제_3.java
@@ -0,0 +1,46 @@
+package 이가원_201702049_W01;
+
+public class 이가원_1주차과제_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int lineNumber ;
+	      lineNumber = 1;
+	      System.out.println(lineNumber +". 야밤 중 불빛이");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 발갛게 어렴풋이 보여라");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +".");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 들리는 듯 마는 듯");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 발자국 소리");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". ");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 스러져 ");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 가는 발자국 소리");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". ");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 아무리");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 혼자 누워 몸을 뒤척여도");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". ");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 잃어버린 ");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 잠은 다시 안 오라");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". ");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 야밤 중 불빛이");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". 발갛게 어렴풋이 보여라");
+	      lineNumber = lineNumber + 1;
+	      System.out.println(lineNumber +". -김소월-");
+	}
+
+}
